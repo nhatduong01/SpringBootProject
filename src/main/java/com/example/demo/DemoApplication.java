4b12d1e7d6f84162631ff87e28ef646da726a4a6
DuongNguyen <nhatduong30001@gmail.com>
2022-03-06 20:23:47 +0700

    Add data JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +12,12 @@ public class DemoApplication {
 	{
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository)
+	{
+		return args -> {
+			Student maria = new Student("Maria", "Johnson", "maria@gmail.com", 21);
+			studentRepository.save(maria);
+		};
+	}
 }
