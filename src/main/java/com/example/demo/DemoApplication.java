8ddc326d38f392222f9506971ae4af8e0f884e1d
DuongNguyen <nhatduong30001@gmail.com>
2022-03-09 15:17:56 +0700

    Init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,13 +11,5 @@ public class DemoApplication {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	@Bean
-	CommandLineRunner commandLineRunner(StudentRepository studentRepository)
-	{
-		return args -> {
-			Student maria = new Student("Maria", "Johnson", "maria@gmail.com", 21);
-			studentRepository.save(maria);
-		};
 	}
 }
